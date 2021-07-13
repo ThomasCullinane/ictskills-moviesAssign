33cc3e3970936ee7a0289a6add47678a5b80ec79
Thomas Cullinane <59512621+ThomasCullinane@users.noreply.github.com>
2021-07-13 22:21:23 +0100

    Movie Details Page rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import HomePage from "./pages/homePage";
+import MovieDetailsPage from './pages/movieDetailsPage'
+
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
+
+const App = () => {
+  return (
+      <MovieDetailsPage movie={sample} images={images} />
+      );
+};
 
 const sample = {
   adult: false,
@@ -85,10 +99,6 @@ const sample = {
 
 const movies = [sample, sample, sample, sample, sample, sample, sample];
 
-const App = () => {
-  return (
-      <HomePage movies={movies} />
-  );
-};
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
