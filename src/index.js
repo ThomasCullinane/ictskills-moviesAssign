09e8b4feb6c4805ca53ea036163b380876b068d1
Thomas Cullinane <59512621+ThomasCullinane@users.noreply.github.com>
2021-07-18 17:49:57 +0100

    Full review page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
+import MovieReviewPage from "./pages/movieReviewPage";
 
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 
@@ -18,6 +19,7 @@ const App = () => {
         </li>
       </ul>
       <Switch>
+        <Route path="/reviews/:id" component={MovieReviewPage} />
         <Route exact path="/movies/favourites" component={FavouriteMoviesPage} />
         <Route path="/movies/:id" component={MoviePage} />
         <Route exact path="/" component={HomePage} />
